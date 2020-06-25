329c64ce97249f252dc2c6362aa4901858c298c3
PedroLucasSS <pedro.lucas4431@gmail.com>
2020-06-25 18:17:21 -0300

    desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+import TransAcetionReporitory from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionReporitory = getCustomRepository(TransAcetionReporitory);
+    const categoryReporitory = getRepository(Category);
+
+    const { total } = await transactionReporitory.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('Total value exceeded');
+    }
+
+    let categoryId = await categoryReporitory.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryId) {
+      categoryId = categoryReporitory.create({
+        title: category,
+      });
+
+      await categoryReporitory.save(categoryId);
+    }
+
+    const transaction = transactionReporitory.create({
+      title,
+      value,
+      type,
+      category: categoryId,
+    });
+
+    await transactionReporitory.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    const findTransaction = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!findTransaction) {
+      throw new AppError('Email address already used.');
+    }
+
+    await transactionRepository.remove(findTransaction);
   }
 }
 
